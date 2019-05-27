19b62853e50fe4bdade1403d3683efda02773651
masterUNG <phrombutr@gmail.com>
2019-05-27 10:09:07 +0700

    Var and Constant

---
+++ VarAndConstant.playground/Contents.swift
@@ -0,0 +1,20 @@
+//การประกาศ ตัวแปรแบบกำหนดค่าได้
+var myName = "มาสเตอร์ อึ่ง"    // นี่คือการประกาศตัวแปรแบบให้ value เป็นตัวกำหนด data type
+var myComany: String = "EWTC"   //นี่คือการประกาศตัวแปร แบบ กำหนด Datatype เลย
+
+var myNumber = 123  // For Integer
+var myNumber2: Int = 456 // Assign DataType
+
+var myNumber3 = 12.34 //For Double
+var myNumber4: Double = 45.67   // Assign DataType
+
+var status = true   //For Booleane
+var status2: Bool = false   // Assign DataType
+
+
+
+
+
+
+
+
